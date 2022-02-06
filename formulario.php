122d050a51dd11b6c18955f6271f00b7ed4bdd49
Mesaque <63453751+titiushadow@users.noreply.github.com>
2022-02-06 14:38:17 -0300

    Delete formulario.php

--- formulario.php
+++
@@ -1,8 +0,0 @@
-<?php
-     $servidor = "localhost";
-     $user = "root";
-     $password = "";
-     $dbname = "Cadastro_curriculo";
-
-     $conn = mysqli_connect("$servidor, $user, $password, $dbname")
-?>
